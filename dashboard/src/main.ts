fbe988ad23de4463e0191ba70cfeb5ed75f096cb
Ali Mawiya <amawiya55@gmail.com>
2026-06-11 03:51:20 +0530

    Update Cors Origin

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   await app.listen(port);
   app.enableCors({
     origin: 'http://127.0.0.1:5500',
-    Credential: true
+    credential: true
   });
 }
 bootstrap();
