00919628111a7520eefa6e067081d535afb0be7a
Ali Mawiya <amawiya55@gmail.com>
2026-06-11 10:29:30 +0530

    Update Cors Origin

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -20,12 +20,13 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('dashboard-management', app, document);
   const port = configService.get<string>('PORT') || '0.0.0.0';
-  await app.listen(port);
   app.enableCors({
     origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
+  await app.listen(port);
+
 }
 bootstrap();
